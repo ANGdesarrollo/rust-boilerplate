d9d43b60de563e62301cbd294fb53c25ad1fbbb6
Alexis Graff <alexisgraff123@gmail.com>
2024-05-12 23:16:45 -0300

    feat: implementing sea orm and table migration (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use actix_web::{App, HttpServer, web};
+use actix_web::{App, HttpServer};
+extern crate dotenv;
+use dotenv::dotenv;
 mod modules;
+mod shared;
 
 use crate::modules::item::presentation::routes::init_item_router;
+use crate::shared::infrastructure::create_sea_orm_connection::init;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let _db = init().await;
+    dotenv().ok();
     HttpServer::new(|| {
         App::new()
             .configure(init_item_router)
--- src/modules/item/mod.rs
+++ src/modules/item/mod.rs
@@ -1,3 +1,3 @@
 pub(crate) mod presentation;
-mod infrastructure;
+pub(crate) mod infrastructure;
 mod domain;
